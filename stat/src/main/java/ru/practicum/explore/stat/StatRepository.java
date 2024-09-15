9095b35e1678cfe605cc1f8af758f48b5aa3c8a0
Vichy <andrbowname@yandex.ru>
2024-09-15 19:54:26 -0300

    add stat

--- stat/src/main/java/ru/practicum/explore/stat/StatRepository.java
+++ stat/src/main/java/ru/practicum/explore/stat/StatRepository.java
@@ -11,6 +11,7 @@ public interface StatRepository extends JpaRepository<EndpointHit, Integer> {
 
     @Query("SELECT h FROM EndpointHit h WHERE h.uri = ?3 AND h.timestamp > ?1 AND h.timestamp < ?2")
     List<EndpointHit> findInfoByUri(Timestamp start, Timestamp end, String uri);
+
     @Query("SELECT h FROM EndpointHit h WHERE h.id IN (SELECT MIN(eh.id) FROM EndpointHit eh WHERE eh.uri = ?3 AND eh.timestamp > ?1 AND eh.timestamp < ?2 GROUP BY eh.ip)")
     List<EndpointHit> findInfoByUriUnique(Timestamp start, Timestamp end, String uri);
 }
